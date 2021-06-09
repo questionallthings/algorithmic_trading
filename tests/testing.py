d1d6f2db10f2681e35d8fd0bd9ae6afdf8a9b2c5
questionallthings <questionallthings@yahoo.com>
2021-06-09 06:40:09 -0400

    Created basic testing.py file for testing purposes only.

---
+++ tests/testing.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+'''
+This script is used to test various ideas from internal/external sources before implementing in development code.
+This will not be part of final package.
+'''
