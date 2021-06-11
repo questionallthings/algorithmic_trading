f2ba28a3b4121d956fff6300fa5e58e5d3642788
questionallthings <questionallthings@yahoo.com>
2021-06-11 06:40:51 -0400

    Updated stock list file with stock info beyond stock name.

--- src/__init__.py
+++ src/__init__.py
@@ -8,6 +8,7 @@ import os
 import math
 
 import alpaca_trade_api
+import pandas as pd
 
 import historical_stock_data_manager
 import strategies
@@ -17,6 +18,11 @@ stock_data = {}
 stock_files_directory = 'stocks/'
 trade_api = alpaca_trade_api.REST()
 account = trade_api.get_account()
+
+pd.set_option('max_columns', 999)
+pd.set_option('max_colwidth', 999)
+pd.set_option('max_rows', 999)
+pd.set_option('display.expand_frame_repr', False)
 
 
 class StockData:
@@ -44,7 +50,7 @@ def set_parser():
     exclusive_group.add_argument('-l', '--live',
                                  action='store_const', dest='run', const='live',
                                  help='This will run the program live with no back testing or update.')
-    parser.add_argument('-s', '--strategy', choices=strategy_options, default='ep', nargs='*',
+    parser.add_argument('-s', '--strategy', choices=strategy_options, default='ss', nargs='*',
                         help='\n'.join(f'{key}: {value}' for key, value in strategy_options.items()),
                         metavar='S')
     parser.add_argument('-v', '--version', action='version', version='Algorithmic Trading 0.0.1')
@@ -55,9 +61,11 @@ def set_parser():
                                help='Maximum current closing value.')
     filter_parser.add_argument('-avg_30', dest='avg_30_volume', type=int,
                                help='Average volume over 30 days.')
+    filter_parser.add_argument('-quote_type', dest='quote_type', type=str, default='EQUITY',
+                               help='What type of stock to filter for.')
     parser.set_defaults(run='live',
-                        close_min=5,
-                        close_max=100,
+                        close_min=1,
+                        close_max=5,
                         avg_30_volume=1000000)
 
     return parser.parse_args()
@@ -65,7 +73,7 @@ def set_parser():
 
 def filter_stock_list(filter_options):
     print(f'{datetime.datetime.now()} :: Importing stock data files.')
-    historical_stock_data_manager.import_data(stock_data)
+    historical_stock_data_manager.import_data(stock_data, filter_options.quote_type)
     print(f'{datetime.datetime.now()} :: Imported {len(stock_data)} files.')
     for key, value in list(stock_data.items()):
         if (value.daily_stock_data.volume.iloc[-31:-1].min() < filter_options.avg_30_volume) or \
@@ -104,16 +112,16 @@ def order(symbol, risk, price, reward):
     test = trade_api.get_last_trade(symbol=symbol)
     if price > test.price > risk:
         print(f'Stock - {symbol} :: Risk - {risk} :: Price - {price} :: Last Trade - {test.price}:: Reward - {reward}')
-        trade_api.submit_order(symbol=symbol,
-                               side='buy',
-                               type='stop',
-                               stop_price=price,
-                               qty=math.floor((float(account.cash) * .01) / price),
-                               time_in_force='gtc',
-                               order_class='bracket',
-                               take_profit=dict(limit_price=reward),
-                               stop_loss=dict(stop_price=risk,
-                                              limit_price=str(round(risk * .99, 2))))
+        #trade_api.submit_order(symbol=symbol,
+        #                       side='buy',
+        #                       type='stop',
+        #                       stop_price=price,
+        #                       qty=math.floor((float(account.cash) * .01) / price),
+        #                       time_in_force='day',
+        #                       order_class='bracket',
+        #                       take_profit=dict(limit_price=reward),
+        #                       stop_loss=dict(stop_price=risk,
+        #                                      limit_price=str(round(risk * .99, 2))))
 
 
 def main():
--- src/historical_stock_data_manager.py
+++ src/historical_stock_data_manager.py
@@ -1,20 +1,25 @@
 from concurrent import futures
 import yahooquery as yq
 import pandas as pd
+import json
+import itertools
 
 stock_files_directory = 'stocks/'
 stock_list_file = 'stock_list.txt'
 
 
-def import_stock_data(stock_name):
+def import_stock_data(stock_name, type):
+    print(type)
     dataframe_import = pd.read_csv(filepath_or_buffer=f'{stock_files_directory}{stock_name}_daily_stock_data.txt',
                                    sep=',')
     return dataframe_import
 
 
-def import_data(stock_data):
+def import_data(stock_data, quote_type):
     with futures.ProcessPoolExecutor(max_workers=2) as import_executor:
-        for each_stock, stock_list_results in zip(stock_data, import_executor.map(import_stock_data, stock_data)):
+        for each_stock, stock_list_results in zip(stock_data, import_executor.map(import_stock_data,
+                                                                                  stock_data,
+                                                                                  itertools.repeat(quote_type))):
             stock_data[each_stock].daily_stock_data = stock_list_results
 
 
@@ -34,10 +39,26 @@ def update_data():
         update_executor.map(update_stock_data, update_list)
 
 
-def update_stock_list_data():
-    pass
+def update_stock_list_data(stock):
+    yahoo_query_data = yq.Ticker(stock)
+    stock_info_data = yahoo_query_data.quote_type
+
+    return stock_info_data
 
 
 def update_stock_list():
-    pass
+    new_stock_data = []
+    update_list = []
+    with open(stock_list_file, 'r') as file:
+        for each_ticker in file:
+            ticker_data = json.loads(each_ticker)
+            if 'symbol' in ticker_data:
+                update_list.append(ticker_data['symbol'])
+    with futures.ThreadPoolExecutor() as update_executor:
+        for each_stock, update_list_results in zip(update_list, update_executor.map(update_stock_list_data, update_list)):
+            new_stock_data.append(update_list_results)
+    with open(stock_list_file, 'w') as file:
+        for each_ticker in new_stock_data:
+            for each_value in each_ticker.values():
+                file.writelines(f'{json.dumps(each_value)}\n')
 
