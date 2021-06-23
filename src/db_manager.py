59d4ed80b6b028233cbdafcde9bf995c089d5128
questionallthings <questionallthings@yahoo.com>
2021-06-23 14:10:01 -0400

    added minute bar gathering functionality

--- src/db_manager.py
+++ src/db_manager.py
@@ -86,7 +86,39 @@ class Database:
         sql_server.commit()
 
     def update_minute_bars(self, stock):
-        pass
+        yahoo_query_data = yq.Ticker(stock)
+        daily_stock_data = yahoo_query_data.history(period='1d',
+                                                    interval='1m').round(4)
+        sql_server = pymysql.connect(host=self.memsql_host,
+                                     user=self.memsql_user,
+                                     password=self.memsql_password,
+                                     port=int(self.memsql_port),
+                                     database=self.database_name,
+                                     cursorclass=pymysql.cursors.DictCursor)
+        daily_stock_data.reset_index(level=[0, 1], inplace=True)
+        daily_stock_data.set_index('date', inplace=True)
+        daily_stock_data.fillna('NULL')
+        columns = daily_stock_data.columns
+        for each_date in daily_stock_data.index:
+            try:
+                query_text = f'INSERT INTO minute_bars (date, '
+                for each_column in columns[:-1]:
+                    query_text += f'{each_column}, '
+                query_text += f'{columns[-1]}) VALUES (\'{datetime.strftime(each_date, "%Y-%m-%d")}\', '
+                for each_column in columns[:-1]:
+                    if each_column == 'symbol':
+                        query_text += f'\'{daily_stock_data.loc[each_date].loc[each_column]}\', '
+                    else:
+                        query_text += f'{daily_stock_data.loc[each_date].loc[each_column]}, '
+                query_text += f'{daily_stock_data.loc[each_date].loc[columns[-1]]})'
+                print(query_text)
+                with sql_server.cursor() as daily_bars_post:
+                    daily_bars_post.execute(f'{query_text}')
+                    results = daily_bars_post.fetchall()
+                    print(results)
+            except:
+                pass
+        sql_server.commit()
 
 
 if __name__ == '__main__':
@@ -112,16 +144,20 @@ if __name__ == '__main__':
         memsql_stock_info_list.append(each['symbol'])
 
     # # # UPDATING STOCK INFO
-    for each in tradeable_assets:
-        alpaca_tradeable_assets.append(each.symbol)
-    for each_stock in alpaca_tradeable_assets:
-        if each_stock not in memsql_stock_info_list:
-            stock_info_update_list.append(each_stock)
-    with futures.ThreadPoolExecutor() as info_executor:
-        info_executor.map(database.update_stock_info, stock_info_update_list)
+    #for each in tradeable_assets:
+    #    alpaca_tradeable_assets.append(each.symbol)
+    #for each_stock in alpaca_tradeable_assets:
+    #    if each_stock not in memsql_stock_info_list:
+    #        stock_info_update_list.append(each_stock)
+    #with futures.ThreadPoolExecutor() as info_executor:
+    #    info_executor.map(database.update_stock_info, stock_info_update_list)
 
     # # # UPDATING DAILY BARS
+    #with futures.ThreadPoolExecutor() as daily_executor:
+    #    daily_executor.map(database.update_daily_bars, memsql_stock_info_list)
+
+    # # # UPDATING MINUTE BARS
     with futures.ThreadPoolExecutor() as daily_executor:
-        daily_executor.map(database.update_daily_bars, memsql_stock_info_list)
+        daily_executor.map(database.update_minute_bars, memsql_stock_info_list)
 
     print(f'Total time for updating: {datetime.now() - start_time}')
