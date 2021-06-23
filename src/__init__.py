ffcea3099af9d37cd709d3e8fde341f494b2acc4
questionallthings <questionallthings@yahoo.com>
2021-06-23 14:50:30 -0400

    added minute bar gathering functionality

--- src/__init__.py
+++ src/__init__.py
@@ -28,6 +28,21 @@ pd.set_option('max_colwidth', 999)
 pd.set_option('max_rows', 999)
 pd.set_option('display.expand_frame_repr', False)
 
+run_options = ['live', 'backtest', 'strategy']
+strategy_options = ['stochastic_supertrend', 'rsi_stochastic_200ema', 'ichimoku']
+period_options = ['1mo', '1y', 'max']
+timeframe_options = ['5m', '60m', '1d']
+type_options = ['ALL', 'EQUITY', 'ETF']
+
+arguments = {'run': run_options[0],
+             'period': period_options[2],
+             'timeframe': timeframe_options[2],
+             'strategy': strategy_options[2],
+             'close_min': 1,
+             'close_max': 100,
+             'avg_30_volume': 1000000,
+             'quote_type': type_options[1]}
+
 
 class Stock:
     def __init__(self, data=None, info=None):
@@ -36,68 +51,15 @@ class Stock:
         self.minutes = ''
 
     def set_data(self):
-        self.data.loc[:, 'strategy'] = False
-        self.data.loc[:, 'backtest_profit'] = 0.0
-        self.data.loc[:, 'buy_price'] = np.nan
-        self.data.loc[:, 'sell_price'] = np.nan
-        self.data.loc[:, 'risk'] = 0.0
-        self.data.loc[:, 'reward'] = 0.0
+        self.data['strategy'] = False
+        self.data['backtest_profit'] = 0.0
+        self.data['buy_price'] = np.nan
+        self.data['sell_price'] = np.nan
+        self.data['risk'] = 0.0
+        self.data['reward'] = 0.0
 
 
-def set_parser():
-    run_options = ['live',
-                   'backtest',
-                   'strategy']
-    strategy_options = ['stochastic_supertrend',
-                        'rsi_stochastic_200ema',
-                        'ichimoku']
-    period_options = ['1mo',
-                      '1y',
-                      'max']
-    timeframe_options = ['5m',
-                         '60m',
-                         '1d']
-    type_options = ['ALL',
-                    'EQUITY',
-                    'ETF']
-    parser = ArgumentParser(formatter_class=RawTextHelpFormatter)
-    subparsers = parser.add_subparsers()
-    parser.add_argument('-r', '--run', choices=run_options,
-                        help='\n'.join(f'{run_item}' for run_item in run_options),
-                        metavar='R')
-    parser.add_argument('-s', '--strategy', choices=strategy_options,
-                        help='\n'.join(f'{strategy_item}' for strategy_item in strategy_options),
-                        metavar='S')
-    parser.add_argument('-p', '--period', choices=period_options,
-                        help='\n'.join(f'{period_item}' for period_item in period_options),
-                        metavar='P')
-    parser.add_argument('-t', '--timeframe', choices=timeframe_options,
-                        help='\n'.join(f'{timeframe_item}' for timeframe_item in timeframe_options),
-                        metavar='T')
-    parser.add_argument('-v', '--version', action='version', version='Algorithmic Trading 0.0.1')
-    filter_parser = subparsers.add_parser('filter', help='filter -h')
-    filter_parser.add_argument('-min', dest='close_min', type=int,
-                               help='Minimum current closing value.')
-    filter_parser.add_argument('-max', dest='close_max', type=int,
-                               help='Maximum current closing value.')
-    filter_parser.add_argument('-avg_30', dest='avg_30_volume', type=int,
-                               help='Average volume over 30 days.')
-    filter_parser.add_argument('-quote_type', '--quote_type', choices=type_options,
-                               help='\n'.join(f'{type_item}' for type_item in type_options),
-                               metavar='Q')
-    parser.set_defaults(run=run_options[0],
-                        period=period_options[2],
-                        timeframe=timeframe_options[2],
-                        strategy=strategy_options[2],
-                        close_min=1,
-                        close_max=100,
-                        avg_30_volume=1000000,
-                        quote_type=type_options[1])
-
-    return parser.parse_args()
-
-
-def import_filter_stocks(connection, filter_options):
+def import_filter_stocks(connection):
     print(f'{datetime.now()} :: Importing stock data.')
     stock_list = []
     with connection.cursor() as filter_query:
@@ -105,8 +67,8 @@ def import_filter_stocks(connection, filter_options):
                              f'WHERE date BETWEEN '
                              f'\'{datetime.strftime(datetime.today() - timedelta(days=5), "%Y-%m-%d")}\' '
                              f'AND \'{datetime.strftime(datetime.today() - timedelta(days=1), "%Y-%m-%d")}\' '
-                             f'AND close BETWEEN {filter_options.close_min} AND {filter_options.close_max} '
-                             f'GROUP BY symbol HAVING AVG(volume) > {filter_options.avg_30_volume}')
+                             f'AND close BETWEEN {arguments["close_min"]} AND {arguments["close_max"]} '
+                             f'GROUP BY symbol HAVING AVG(volume) > {arguments["avg_30_volume"]}')
         filter_query_results = filter_query.fetchall()
     for each_stock in filter_query_results:
         if each_stock['symbol'] in stock_data:
@@ -132,7 +94,7 @@ def run_backtest(strategy_stock_data):
                                index=False)
 
 
-def test_strategy(connection, stock, strategy, arguments):
+def test_strategy(connection, stock, strategy):
     print(f'{datetime.now()} :: Importing test stock data.')
     sql_df = pd.read_sql_query(f'SELECT * FROM daily_bars WHERE symbol=\'{stock}\'', connection)
     sql_df.set_index(pd.to_datetime(sql_df.date), inplace=True)
@@ -190,14 +152,14 @@ def test_strategy(connection, stock, strategy, arguments):
              title=f'{stock}')
 
 
-def run_strategy(strategy, arguments):
+def run_strategy(strategy):
     with futures.ProcessPoolExecutor(max_workers=2) as indicator_executor:
         for each_stock, stock_list_results in zip(stock_data,
                                                   indicator_executor.map(getattr(strategies, strategy),
                                                                          stock_data.items(),
                                                                          repeat(arguments))):
             stock_data[each_stock].data = stock_list_results
-    if arguments.run == 'live':
+    if arguments['run'] == 'live':
         for each_stock, value in list(stock_data.items()):
             if not stock_data[each_stock].data.strategy.iloc[-1] or \
                     stock_data[each_stock].data.risk.iloc[-1] == 0.0 or \
@@ -206,7 +168,7 @@ def run_strategy(strategy, arguments):
 
 
 def order(stock_data_order, symbol):
-    order_status = False
+    status = False
     test = trade_api.get_last_trade(symbol=symbol)
     if stock_data_order.buy_price >= test.price > stock_data_order.risk:
         print(f'Stock - {symbol} :: '
@@ -227,13 +189,12 @@ def order(stock_data_order, symbol):
                                               limit_price=str(round(stock_data_order.risk * .99, 2))))
         '''
 
-    return order_status
+    return status
 
 
 if __name__ == "__main__":
     start_time = datetime.now()
     print(f'{datetime.now()} :: Starting')
-    arguments = set_parser()
     print(f'{datetime.now()} :: Using the following arguments: {arguments}.')
     database = db_manager.Database()
     memsql_server = pymysql.connect(host=database.memsql_host,
@@ -242,31 +203,30 @@ if __name__ == "__main__":
                                     port=int(database.memsql_port),
                                     database=database.database_name,
                                     cursorclass=pymysql.cursors.DictCursor)
-    if arguments.run == 'strategy':
+    if arguments['run'] == 'strategy':
         test_strategy(connection=memsql_server,
                       stock='HPQ',
-                      strategy=arguments.strategy,
-                      arguments=arguments)  # HPQ is used due to largest set
+                      strategy=arguments['strategy'])  # HPQ is used due to largest set
     else:
         with memsql_server.cursor() as db_query:
             db_query.execute('SELECT * FROM stock_info')
             memsql_stock_list_result = db_query.fetchall()
         for each_result in memsql_stock_list_result:
             stock_data[each_result['symbol']] = Stock(info=each_result)
-        if arguments.quote_type != 'ALL':
+        if arguments['quote_type'] != 'ALL':
             for each_stock, value in list(stock_data.items()):
-                if stock_data[each_stock].info['quoteType'] != arguments.quote_type:
+                if stock_data[each_stock].info['quoteType'] != arguments['quote_type']:
                     del stock_data[each_stock]
-        print(f'{datetime.now()} :: Running {arguments.run}.')
-        import_filter_stocks(memsql_server, arguments)
+        print(f'{datetime.now()} :: Running {arguments["run"]}.')
+        import_filter_stocks(memsql_server)
         print(f'{datetime.now()} :: Filtered down to {len(stock_data)} stocks.')
-        run_strategy(arguments.strategy, arguments)
-        print(f'{datetime.now()} :: Strategy \'{arguments.strategy}\' filtered list down to '
+        run_strategy(arguments['strategy'])
+        print(f'{datetime.now()} :: Strategy \'{arguments["strategy"]}\' filtered list down to '
               f'{len(stock_data)} stock(s).')
-        if arguments.run == 'backtest':
+        if arguments['run'] == 'backtest':
             for each_stock in stock_data:
                 run_backtest(stock_data[each_stock].data)
-        elif arguments.run == 'live':
+        elif arguments['run'] == 'live':
             orders = []
             current_orders = trade_api.list_orders()
             for each_order in current_orders:
